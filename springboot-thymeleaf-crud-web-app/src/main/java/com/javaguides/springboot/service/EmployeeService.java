a00f718619a350482e8b8bd9c2fa978d476a0425
Aamir <aamirp1@outlook.com>
2021-04-06 18:35:53 +0100

    employee management system

---
+++ springboot-thymeleaf-crud-web-app/src/main/java/com/javaguides/springboot/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.javaguides.springboot.service;
+
+import com.javaguides.springboot.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
